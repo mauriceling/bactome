fe789d13d3d9a992a1153c615512f0417c3ebc40
mauriceling <mauriceling@c91b2ec0-32c0-4d91-abfb-a1c177c4065e>
2011-11-23 01:48:21 +0000

    initial import - NormFinder

---
+++ bactome/src/normfinder.py
@@ -0,0 +1,17 @@
+"""
+Python implementation of NormFinder.
+
+NormFinder reference: Andersen CL, Jensen JL, Orntoft TF. 2004.
+Normalization of real-time quantitative reverse transcription-PCR 
+data: a model-based variance estimation approach to identify genes 
+suited for normalization, applied to bladder and colon cancer data 
+sets. Cancer Research 64(15):5245-5250.
+
+Date created: 23rd November 2011
+
+Licence: Python Software Foundation License version 2
+"""
+
+def normfinder(data, header=True):
+    return result
+    
