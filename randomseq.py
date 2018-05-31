7671d50fd1f7156ca06fe601f357c2355a2eed9f
Maurice HT Ling <mauriceling@acm.org>
2018-05-31 15:21:35 +0800

    + added mixed sequence generation

--- randomseq.py
+++ randomseq.py
@@ -410,9 +410,136 @@ def shuffle(sequence):
     random.shuffle(sequence)
     return ''.join(sequence)
 
+def _process_mixed_dictionary(statement):
+    '''
+    Private method - Process the sequence specification statement 
+    from gMixedSequences() function into a dictionary.
+    '''
+    statement = [s.strip() for s in statement.split(';')]
+    d = {}
+    for i in range(len(statement)):
+        d[i] = {'statement': statement[i]}
+    for k in d:
+        if d[k]['statement'].startswith('c'):
+            d[k]['type'] = 'constant'
+            d[k]['sequence'] = d[k]['statement'][2:-1]
+        elif d[k]['statement'].startswith('v'):
+            d[k]['type'] = 'variable' 
+            options = d[k]['statement'][2:-1]
+            options = [x.strip() for x in options.split(',')]
+            d[k]['options'] = options
+            d[k]['sequence'] = None
+    print(d)
+    return d
+
+def _generate_MDseq(o, md):
+    '''
+    Private method - Used by gMixedSequences() function to generate 
+    the required random sequences. 
+    '''
+    for k in md:
+        if md[k]['type'] == 'variable':
+            min_length = md[k]['options'][0]
+            max_length = md[k]['options'][1]
+            allow_start = md[k]['options'][2]
+            allow_stop = md[k]['options'][3]
+            cap_start = md[k]['options'][4]
+            cap_stop = md[k]['options'][5]
+            s = _generate_sequence(o, min_length, max_length, 
+                                      allow_start, allow_stop, 
+                                      cap_start, cap_stop)
+            md[k]['sequence'] = s
+    keys = list(md.keys())
+    keys.sort()
+    sequence = [md[k]['sequence'] for k in keys]
+    return ''.join(sequence)
+
+def gMixedSequences(n, selection='A,250;T,250;G,250;C,250',
+                    start_codons='TTG,CTG,ATG', cap_start=False,
+                    stop_codons='TAA,TAG,TGA', cap_stop=False,
+                    source_seq='', fasta=True, prefix='Test',
+                    statement='v(10,15,False,False,False,False);c(gtccg);v(10,15,False,False,False,False)'):
+    '''!
+    Function to generate a mixed sequence, which can take many forms. 
+    For example, this function can be used to generate a random 
+    sequence of 30-40 nucleotides, followed by a 10-nucleotide 
+    constant region, followed by a random region of exactly 20 
+    nucleotides. 
+
+    Usage:
+
+        python randomseq.py MS --n=10 --start_codons='TTG,CTG,ATG' --stop_codons='TAA,TAG,TGA' --cap_start=True --cap_stop=True --selection=A,250;T,250;G,250;C,250 --fasta=True --prefix='Test' --statement='v(10,15,False,False,False,False);c(gtccg);v(10,15,False,False,False,False)'
+
+    The statement is a specification describing the random sequence to 
+    generate. Each part of the sequence is specified in the format of 
+    v(<min length>, <max length>, <allow start>, <allow stop>, <cap 
+    start>, <cap stop>) or c(<constant sequence>). A random sequence 
+    of 25-35 nucleotides, not allowing either start or stop codons 
+    with the sequence but required capping the 5'-end of the sequence 
+    with start codon is defined as v(25,35,False,False,True,False). A 
+    constant region, 5'-GAATTC-3', is defined as c(GAATTC). Each part 
+    is then concatenated with semi-colon. Hence, the definition, 
+    v(10,10,False,False,True,False);c(GAATTC); v(20,30,False,False, 
+    False,True), will generate a sequence of (1) a random region of 10 
+    nucleotides and 5' capped with a start codon (forming 13 
+    nucleotides), followed by (2) a constant region of GAATTC, 
+    followed by (3) a random 20-30 nucleotide sequence and capped with 
+    stop codon at the 3' end. The resulting sequence will be 13 + 6 + 
+    23 to 33 = 42 to 52 nucleotides long.
+
+    @param n Integer: Number of random sequence(s) to generate.
+    @param selection String: Definition of the atomic sequence(s), 
+    defined as a semi-colon delimited definition of comma-delimited 
+    definition, used for random sequence generation. Default = 
+    'A,250;T,250;G,250;C,250' which means that the list of atomic 
+    sequences is defined as 250 'A', 250 'T', 250 'G', and 250 'C'.
+    @param start_codons String: A comma-delimited list to represent 
+    start codons. This is used for two purposes. Firstly, it can be 
+    used to check the randomly generated sequence to ensure that there 
+    is no start codons within the sequence. Secondly, it can be used 
+    to cap the start of a newly generated sequence.Default = 
+    'TTG,CTG,ATG'.
+    @param cap_start Boolean: Flag to determine whether to cap the 
+    start of the randomly generated sequence with a start codon. 
+    Default = False.
+    @param stop_codons String: A comma-delimited list to represent 
+    stop codons. This is used for two purposes. Firstly, it can be 
+    used to check the randomly generated sequence to ensure that there 
+    is no stop codons within the sequence. Secondly, it can be used 
+    to cap the stop of a newly generated sequence.Default = 
+    'TAA,TAG,TGA'.
+    @param cap_stop Boolean: Flag to determine whether to cap the 
+    end of the randomly generated sequence with a stop codon. 
+    Default = False.
+    @param source_seq String: A sequence to be used to generate the 
+    atomic sequence(s), used for random sequence generation. The 
+    atomic sequence(s) is/are essentially an single-character 
+    alphanumeric of the sequence, where relative abundance of each 
+    character is determined by the occurrence in the sequence.
+    @param fasta Boolean: Flag to determine whether the output is to 
+    be formatted as a FASTA file. Default = True.
+    @param prefix String: Prefix to the running number for the 
+    sequence name, if the output is to be a FASTA format. Default = 
+    'Test'.
+    @param statement String: Specification statement of the random 
+    sequence to generate. Please see above description. Default = 
+    'v(10,15,False,False,False,False);c(gtccg); 
+    v(10,15,False,False,False,False)'
+    '''
+    o = _initial_RandomSequence(start_codons, stop_codons,
+                                selection, source_seq)
+    md = _process_mixed_dictionary(statement)
+    for i in range(int(n)):
+        sequence = _generate_MDseq(o, md)
+        if fasta:
+            title = '_'.join([str(prefix), str(i+1)])
+            print('> %s' % title)
+        print(sequence)
+
 
 if __name__ == '__main__':
     exposed_functions = {'FLS': gFixedLength,
                          'VLS': gVariableLength,
+                         'MS': gMixedSequences,
                          'shuffle': shuffle}
     fire.Fire(exposed_functions)
