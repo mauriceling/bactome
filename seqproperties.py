4fbdb77496432e5ba30c7f371da2eec8aa0eb106
Maurice HT Ling <mauriceling@acm.org>
2018-09-14 12:25:19 +0800

    # improved exception handling for aromaticity

--- seqproperties.py
+++ seqproperties.py
@@ -701,11 +701,17 @@ def aromaticity(fastafile, molecule, genetic_code=1, to_stop=True):
             data = ' : '.join([str(x) for x in data])
             print(data)
         except ZeroDivisionError:
-            data = [k, 'undefined']
-            data = ' : '.join([str(x) for x in data])
+            data = ' : '.join([str(k), 'undefined'])
             print(data)
         except KeyError:
-            pass
+            data = ' : '.join([str(k), 'KeyError'])
+            print(data)
+        except IndexError:
+            data = ' : '.join([str(k), 'IndexError'])
+            print(data)
+        except:
+            data = ' : '.join([str(k), 'Error'])
+            print(data)
 
 def instability(fastafile, molecule, genetic_code=1, to_stop=True):
     '''!
