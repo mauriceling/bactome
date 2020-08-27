07c696a13d14f73b88515bef779742728fca3c78
Maurice Ling <mauriceling@acm.org>
2020-08-28 00:56:51 +0800

    + added new evaluation metrics

--- bactclass.py
+++ bactclass.py
@@ -143,20 +143,25 @@ def cross_validate(classifier, X, Y, fold):
     print("%s fold cross validation" % str(fold))
     print("")
     scores = cross_val_score(classifier, X, Y, cv=fold, scoring="precision")
-    print("Precision:            %0.2f (sigma = %0.3f)" % (scores.mean()*100, scores.std()*100))
+    print("Precision:                     %0.3f (sigma = %0.4f)" % (scores.mean(), scores.std()))
     scores = cross_val_score(classifier, X, Y, cv=fold, scoring="recall")
-    print("Recall:               %0.2f (sigma = %0.3f)" % (scores.mean()*100, scores.std()*100))
+    print("Recall:                        %0.3f (sigma = %0.4f)" % (scores.mean(), scores.std()))
     scores = cross_val_score(classifier, X, Y, cv=fold, scoring="f1")
-    print("F1 Score:             %0.2f (sigma = %0.3f)" % (scores.mean()*100, scores.std()*100))
+    print("F1 Score:                      %0.3f (sigma = %0.4f)" % (scores.mean(), scores.std()))
     scores = cross_val_score(classifier, X, Y, cv=fold, scoring="accuracy")
-    print("Accuracy:             %0.2f (sigma = %0.3f)" % (scores.mean()*100, scores.std()*100))
+    print("Accuracy:                      %0.3f (sigma = %0.4f)" % (scores.mean(), scores.std()))
     scores = cross_val_score(classifier, X, Y, cv=fold, scoring="roc_auc")
-    print("Area Under ROC Curve: %0.2f (sigma = %0.3f)" % (scores.mean()*100, scores.std()*100))
+    print("Area Under ROC Curve (AUC):    %0.3f (sigma = %0.4f)" % (scores.mean(), scores.std()))
     scores = cross_val_score(classifier, X, Y, cv=fold, scoring="jaccard")
-    print("Jaccard Score:        %0.2f (sigma = %0.3f)" % (scores.mean()*100, scores.std()*100))
+    print("Jaccard Similarity:            %0.3f (sigma = %0.4f)" % (scores.mean(), scores.std()))
+    scores = cross_val_score(classifier, X, Y, cv=fold, scoring="normalized_mutual_info_score")
+    print("Normalized Mutual Information: %0.3f (sigma = %0.4f)" % (scores.mean(), scores.std()))
+    scores = cross_val_score(classifier, X, Y, cv=fold, scoring="v_measure_score")
+    print("V-measure:                     %0.3f (sigma = %0.4f)" % (scores.mean(), scores.std()))
+    scores = cross_val_score(classifier, X, Y, cv=fold, scoring="neg_mean_squared_error")
+    print("Mean Square Error (MSE):       %0.3f (sigma = %0.4f)" % (-1*scores.mean(), scores.std()))
     scores = cross_val_score(classifier, X, Y, cv=fold, scoring="r2")
-    print("R-square:             %0.2f (sigma = %0.3f)" % (scores.mean()*100, scores.std()*100))
-    print("Note: Values are shown as percentage.")
+    print("R-square:                      %0.3f (sigma = %0.4f)" % (scores.mean(), scores.std()))
     print("--------- End of Cross Validation Report ------------")
 
 def process_classifier(datafile, label, classifier, oclass, otype, 
@@ -441,6 +446,129 @@ def useSVM(datafile, classfile, classtype, resultfile):
     @param resultfile String: Path to write out the classified results.
     """
     useScikitClassifier("SVM", datafile, classfile, classtype, resultfile)
+
+def generateDT(datafile, label, 
+               oclass="classifier_DT.pickle", 
+               otype="pickle",
+               criterion="gini",
+               splitter="best",
+               classparam=True, 
+               confusion=True, 
+               classreport=True,
+               cross_validation=5):
+    """!
+
+    @param datafile String: Path to CSV data file used to generate SVM.
+    @param label String: Column (field) name in the data file to indicate the class label.
+    @param oclass String: Path to write out the generated classifier. Default = classifier_SVM.pickle
+    @param otype String: Type of file to write out the generated classifier. Allowable types are "pickle" and "joblib". Default = pickle
+    @param criterion String: The function to measure the quality of a split. Allowable types are "gini" (Gini impurity) and "entropy" (information gain). Default = gini
+    @param splitter String: The strategy used to choose the split at each node. Allowable types are "best" (best split) and "random" (best random split). Default = best
+
+max_depth int, default=None
+
+    The maximum depth of the tree. If None, then nodes are expanded until all leaves are pure or until all leaves contain less than min_samples_split samples.
+min_samples_split int or float, default=2
+
+    The minimum number of samples required to split an internal node:
+
+        If int, then consider min_samples_split as the minimum number.
+
+        If float, then min_samples_split is a fraction and ceil(min_samples_split * n_samples) are the minimum number of samples for each split.
+
+    Changed in version 0.18: Added float values for fractions.
+min_samples_leaf int or float, default=1
+
+    The minimum number of samples required to be at a leaf node. A split point at any depth will only be considered if it leaves at least min_samples_leaf training samples in each of the left and right branches. This may have the effect of smoothing the model, especially in regression.
+
+        If int, then consider min_samples_leaf as the minimum number.
+
+        If float, then min_samples_leaf is a fraction and ceil(min_samples_leaf * n_samples) are the minimum number of samples for each node.
+
+    Changed in version 0.18: Added float values for fractions.
+min_weight_fraction_leaf float, default=0.0
+
+    The minimum weighted fraction of the sum total of weights (of all the input samples) required to be at a leaf node. Samples have equal weight when sample_weight is not provided.
+max_features int, float or {"auto", "sqrt", "log2"}, default=None
+
+    The number of features to consider when looking for the best split:
+
+            If int, then consider max_features features at each split.
+
+            If float, then max_features is a fraction and int(max_features * n_features) features are considered at each split.
+
+            If "auto", then max_features=sqrt(n_features).
+
+            If "sqrt", then max_features=sqrt(n_features).
+
+            If "log2", then max_features=log2(n_features).
+
+            If None, then max_features=n_features.
+
+    Note: the search for a split does not stop until at least one valid partition of the node samples is found, even if it requires to effectively inspect more than max_features features.
+random_state int, RandomState instance, default=None
+
+    Controls the randomness of the estimator. The features are always randomly permuted at each split, even if splitter is set to "best". When max_features < n_features, the algorithm will select max_features at random at each split before finding the best split among them. But the best found split may vary across different runs, even if max_features=n_features. That is the case, if the improvement of the criterion is identical for several splits and one split has to be selected at random. To obtain a deterministic behaviour during fitting, random_state has to be fixed to an integer. See Glossary for details.
+max_leaf_nodes int, default=None
+
+    Grow a tree with max_leaf_nodes in best-first fashion. Best nodes are defined as relative reduction in impurity. If None then unlimited number of leaf nodes.
+min_impurity_decrease float, default=0.0
+
+    A node will be split if this split induces a decrease of the impurity greater than or equal to this value.
+
+    The weighted impurity decrease equation is the following:
+
+    N_t / N * (impurity - N_t_R / N_t * right_impurity
+                        - N_t_L / N_t * left_impurity)
+
+    where N is the total number of samples, N_t is the number of samples at the current node, N_t_L is the number of samples in the left child, and N_t_R is the number of samples in the right child.
+
+    N, N_t, N_t_R and N_t_L all refer to the weighted sum, if sample_weight is passed.
+
+    New in version 0.19.
+min_impurity_split float, default=0
+
+    Threshold for early stopping in tree growth. A node will split if its impurity is above the threshold, otherwise it is a leaf.
+
+    Deprecated since version 0.19: min_impurity_split has been deprecated in favor of min_impurity_decrease in 0.19. The default value of min_impurity_split has changed from 1e-7 to 0 in 0.23 and it will be removed in 0.25. Use min_impurity_decrease instead.
+class_weight dict, list of dict or "balanced", default=None
+
+    Weights associated with classes in the form {class_label: weight}. If None, all classes are supposed to have weight one. For multi-output problems, a list of dicts can be provided in the same order as the columns of y.
+
+    Note that for multioutput (including multilabel) weights should be defined for each class of every column in its own dict. For example, for four-class multilabel classification weights should be [{0: 1, 1: 1}, {0: 1, 1: 5}, {0: 1, 1: 1}, {0: 1, 1: 1}] instead of [{1:1}, {2:5}, {3:1}, {4:1}].
+
+    The "balanced" mode uses the values of y to automatically adjust weights inversely proportional to class frequencies in the input data as n_samples / (n_classes * np.bincount(y))
+
+    For multi-output, the weights of each column of y will be multiplied.
+
+    Note that these weights will be multiplied with sample_weight (passed through the fit method) if sample_weight is specified.
+
+ccp_alpha float, default=0.0
+
+    Complexity parameter used for Minimal Cost-Complexity Pruning. The subtree with the largest cost complexity that is smaller than ccp_alpha will be chosen. By default, no pruning is performed.
+
+    @param classparam Boolean: Flag to indicate whether to print out SVM parameters. Default = True
+    @param confusion Boolean: Flag to indicate whether to print out confusion matrix. Default = True
+    @param classreport Boolean: Flag to indicate whether to print out classification report. Default = True
+    @param cross_validation Integer: Number of cross validation (if any) to perform. If less than 2, cross validation will not be carried out. Default = 5
+    """
+    from sklearn.tree import DecisionTreeClassifier
+    print("")
+    print("Task: Generate Decision Tree (DT) Classifier")
+    print("Parameters:")
+    print("    Data File = " + str(datafile))
+    print("    Classification Label = " + str(label))
+    print("    Citerion = " + str(criterion))
+    print("    Splitting Strategy = " + str(splitter))
+
+    print("    Classifier File = " + str(oclass))
+    print("    Classifier File Type = " + str(otype))
+    print("")
+    classifier = DecisionTreeClassifier(criterion=str(criterion),
+                                        splitter=str(splitter))
+    process_classifier(datafile, label, classifier, oclass, otype, 
+                       classparam, confusion, classreport, cross_validation)
+    print("===================== DT Generated =====================")
 
 
 if __name__ == "__main__":
