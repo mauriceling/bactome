61cfe0b09cfa8626900226400a36a3b8aca8a61b
Maurice Ling <mauriceling@acm.org>
2022-08-28 10:09:43 +0800

    + complete runBrain()

--- brainopy.py
+++ brainopy.py
@@ -478,12 +478,19 @@ class brainopy(object):
         """!
         Wrapper method to execute / run the entire brain or part of the brain. If a list of neuron_IDs (represented by neuronList) is not given, the entire brain will be executed / ran. To execute / run part of the brain, neurons (represented by neuron_IDs) for the part of the brain must be given as neuronList.
 
-        Neuron processes / functions [(1) synapse to dendrite transfer function (SDTF), (2) dendrite modulator (DMF), (3) dendrite to neuron transfer function (DNTF), (4) neuron modulator (NMF), (5) neuron to axon transfer function (NATF), (6) axon modulator (AMF), and (7) axon to synapse transfer function (ASTF)] are executed for each neuron, followed by brain maintenance processes / functions [(1) neuronal growth function (NGF), (2) neuronal prune function (NPF), (3) synaptic growth function (SGF), (4) synaptic prune function (SPF), and (5) global maintenance function (GMF)].
+        The steps are as follow:
+            1. Neuron processes / functions [(1) synapse to dendrite transfer function (SDTF), (2) dendrite modulator (DMF), (3) dendrite to neuron transfer function (DNTF), (4) neuron modulator (NMF), (5) neuron to axon transfer function (NATF), (6) axon modulator (AMF), and (7) axon to synapse transfer function (ASTF)] executed for each neuron.
+            2. Synapse modulator (SMF) executed for each synapse.
+            3. Synapse to axon transfer function (SATF) executed for each neuron.
+            Brain maintenance processes / functions [(1) neuronal growth function (NGF), (2) neuronal prune function (NPF), (3) synaptic growth function (SGF), (4) synaptic prune function (SPF), and (5) global maintenance function (GMF)] executed.
 
         @param neuroList List: List of neuron_IDs. Default = []
         """
         if len(neuronList) == 0: neuronList = self.getIDs("neuron_body")
+        synapseList = self.getIDs("synapse_state")
         for neuron_ID in neuronList: self.neuronFunction(neuron_ID)
+        for synapse_state_ID in synapseList: self.mfSynapse(synapse_state_ID)
+        for neuron_ID in neuronList: self.tfSynapseAxon(neuron_ID)
         self.maintenanceFunction()
 
     def inputSignal(self, synapse_state_ID, signal_state):
